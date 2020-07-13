4339b9a19c0cf7868934aa696846abe60104f6f7
Lucas <lucaspascholatti@gmail.com>
2020-07-13 17:21:20 +0200

    Update db.js
    Remove { force: true} at sync in db.js

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const databaseUrl = process.env.DATABASE_URL || 'postgres://:<YOUR_PASSWORD>@loc
 const db = new Sequelize(databaseUrl)
 
 db
-.sync( {force: true} )
+.sync()
 .then(() => console.log('Database schema updated'))
 .catch(console.error);
 
